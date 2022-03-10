1b95f272b11d56d2b8c948dd9465c894f2eca958
Akshaya192 <96040710+Akshaya192@users.noreply.github.com>
2022-03-10 09:21:31 +0000

    n.o in fabonacci seq

--- p2final.c
+++ p2final.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
+void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3)
+{
+  printf("enter six numbers\n");
+  scanf("%f%f%f%f%f%f",x1,y1,x2,y2,x3,y3);
+}
+int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3)
+{
+  if
+}
+void output(float x1, float y1, float x2, float y2,float x3, float y3, int istriangle)
+{
+  printf("")
+}
+
 
--- p3final.c
+++ p3final.c
@@ -1,1 +1,25 @@
-
+#include<stdio.h>
+int input_number()
+{
+  int a;
+  printf("enter a number\n");
+  scanf("%d",&a);
+  return 0;
+}
+int is_prime(int n)
+{
+  int n
+  printf()
+}
+void output(int n, int prime)
+{
+  printf("prime number is : %d\n",n,prime);
+}
+int main()
+{
+  int a, n;
+  a=input();
+  n=is_prime(n);
+  output(n,is_prime);
+  return 0;
+}
