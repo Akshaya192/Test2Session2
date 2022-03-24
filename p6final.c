23ddbf31ae3d0f2e0c59c586a440d0e408e15aff
Akshaya192 <96040710+Akshaya192@users.noreply.github.com>
2022-03-24 09:37:17 +0000

    DISTANCE B/W POINTS

--- p6final.c
+++ p6final.c
@@ -1,17 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+void input_string(char *a)
+{
+  printf("enter the string\n");
+  scanf("%s",a);
+}
 int string_length(char *s)
 {
   int i;
   for(i=0;s[i]!='\0';i++);
   return i;
 }
-
 int string_ncmp(char *s1,char *s2)
 {
+  int n;
   for(int i=0; i<n && s1[i]==s2[i] && s1[i] != '\0';i++);
   return s1[i] - s2[i];
 }
-
-
 int string_index(char *s, char *subs)
 {
   int l1=string_lenght(s);
@@ -23,5 +28,18 @@ int string_index(char *s, char *subs)
       return i;
   return -1;
 }
-
+void output(char *string, char *substring, int index)
+{
+  printf("the index of a substring of a string is : %s",index)
+}
+int main()
+{
+  int i;
+  input_string(a);
+  i=strig_length(s);
+  s1=string_ncmp();
+  s2=string_ncmp();
+  output(string,substring,index);
+  return 0;
+}
 /* Understand and debug the functions and write main */
